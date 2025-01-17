ccb00c90bb3206148352d968da7b1c1fe299a125
AmiteshWaghmare <amiteshkumar5199@gmail.com>
2025-01-17 22:08:41 +0530

    Add files via upload

---
+++ Week.java
@@ -0,0 +1,41 @@
+public class Week {
+
+    public static void main(String[] args){
+
+        int DayOfWeek = 8;
+
+        if(DayOfWeek == 1){
+
+            System.out.println("Monday");
+        }
+        else if(DayOfWeek == 2){
+
+            System.out.println("Tuesday");
+        }
+        else if(DayOfWeek == 3){
+
+            System.out.println("Wednesday");
+        }
+        else if(DayOfWeek == 4){
+
+            System.out.println("Thursday");
+        }
+        else if(DayOfWeek == 5){
+
+            System.out.println("Friday");
+        }
+        else if(DayOfWeek == 6){
+
+            System.out.println("Saturday");
+        }
+        else if(DayOfWeek == 7){
+
+            System.out.println("Sunday");
+        }
+        else{
+
+            System.out.println("Day number must be between 1 to 7 :");
+        }
+    }
+    
+}
